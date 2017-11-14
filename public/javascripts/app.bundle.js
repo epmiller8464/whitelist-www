134fd9c60d801b8bd5a4714b80b5407ea1d94c49
Eric Miller <epmiller8464@gmail.com>
2017-11-14 12:50:27 -0600

    additional data capture

--- public/javascripts/app.bundle.js
+++ public/javascripts/app.bundle.js
@@ -69,27 +69,25 @@
 
 "use strict";
 
-// const geoip = require('geoip-lite')
 
 var publicIp = __webpack_require__(1);
 
-function geoLocate(cb) {
-  publicIp.v4().then(function (ip) {
-    return cb(ip);
-  }).catch(function (e) {
-    console.error(e);
-  });
-}
-
 var App = {
-  geoLocate: geoLocate,
+  geoLocate: function geoLocate(cb) {
+    publicIp.v4().then(function (ip) {
+      return cb(null, ip);
+    }).catch(function (e) {
+      return cb(e, null);
+    });
+  },
   validate: function validate() {
     $('.has-error').removeClass('has-error');
-    var firstName = $('input[name=firstName]').val();
-    var lastName = $('input[name=lastName]').val();
-    var email = $('input[name=email]').val();
-    var confirmEmail = $('input[name=confirm-email]').val();
-    var regex = new RegExp(/.+\@.+\..+/);
+    var firstName = $('input[name=firstName]').val(),
+        lastName = $('input[name=lastName]').val(),
+        email = $('input[name=email]').val(),
+        confirmEmail = $('input[name=confirm-email]').val(),
+        regex = new RegExp(/.+\@.+\..+/),
+        isValid = true;
 
     firstName = firstName && firstName.length > 0 ? firstName.trim() : null;
     lastName = lastName && lastName.length > 0 ? lastName.trim() : null;
@@ -97,31 +95,32 @@ var App = {
     confirmEmail = confirmEmail && confirmEmail.length > 0 ? confirmEmail.trim() : null;
     if (!firstName || firstName.length < 2) {
       $('input[name=firstName]').parent('.form-group').addClass('has-error');
-      return false;
+      isValid = false;
     }
 
     if (!lastName || lastName.length < 2) {
       $('input[name=lastName]').parent('.form-group').addClass('has-error');
-      return false;
+      isValid = false;
     }
     if (!email || !regex.test(email)) {
       $('input[name=email]').parent('.form-group').addClass('has-error');
-      return false;
+      isValid = false;
     }
     if (!regex.test(confirmEmail)) {
       $('input[name=confirm-email]').parent('.form-group').addClass('has-error');
-      return false;
+      isValid = false;
     }
     if (email !== confirmEmail) {
       $('input[name=confirm-email]').parent('.form-group').addClass('has-error');
-      return false;
+      isValid = false;
     }
-    return true;
+    return isValid;
   },
   submit: function submit() {
     if (this.validate()) {
-      alert('ready');
-    }
+      // alert('ready')
+      $('body').addClass('loading');
+    } else {}
   }
 };
 window.App = window.App || App;
