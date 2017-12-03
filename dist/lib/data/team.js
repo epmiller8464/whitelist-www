3cb95a83d80308115032523e1d6792dff9213c8a
Eric Miller <epmiller8464@gmail.com>
2017-12-03 14:23:35 -0600

    partners section

--- dist/lib/data/team.js
+++ dist/lib/data/team.js
@@ -6,31 +6,32 @@ module.exports = {
     title: 'Founder',
     subtitle: 'MIT Media Lab and ID3',
     img_path: '/images/team/clippinger.jpg',
-    linked_in: ''
+    linked_in: 'https://www.linkedin.com/in/john-henry-clippinger-5ab1b/'
   }, {
     name: 'John Redpath',
     title: 'Founder',
     subtitle: 'CEO, Trailstone Group',
     img_path: '/images/team/redpath.jpg',
-    linked_in: ''
+    linked_in: 'https://www.linkedin.com/in/john-redpath-1803a37a/'
   }, {
     name: 'Evan Caron',
     title: 'Founder / Managing Director',
     subtitle: 'Renewable Energy Expert and Entrepreneur',
     img_path: '/images/team/caron.jpg',
-    linked_in: ''
+    linked_in: 'https://www.linkedin.com/in/evancaron/'
+
   }, {
     name: 'Youngsook Park',
     title: 'Director of Business Development',
     subtitle: 'Chair of Millenium Project Korea',
     img_path: '/images/team/park.jpg',
-    linked_in: ''
+    linked_in: 'https://www.linkedin.com/in/youngsook-park-mrs-b33468/'
   }, {
     name: 'Andrew Pearsons',
     title: 'Director of Operations',
     subtitle: 'Strategy and Operations Expert',
     img_path: '/images/team/pearsons.jpg',
-    linked_in: ''
+    linked_in: 'https://www.linkedin.com/in/andrew-pearsons-2219079/'
   }, {
     name: 'Eric Miller',
     title: 'Lead Engineer',
@@ -43,49 +44,52 @@ module.exports = {
     title: 'MIT Media Lab – Director, City Sciences',
     subtitle: '',
     img_path: '/images/team/larson.jpg',
-    linked_in: ''
+    linked_in: 'https://www.linkedin.com/in/kent-larson-536079a/'
+
   }, {
     name: 'Tony Seba',
     title: 'Stanford University – Clean Energy Expert',
     subtitle: '',
     img_path: '/images/team/seba.jpg',
-    linked_in: ''
+    linked_in: 'https://www.linkedin.com/in/tonyseba/'
+
   }, {
     name: 'Brad Hardin',
     title: 'CTO, Black and Veatch',
     subtitle: '',
     img_path: '/images/team/hardin.jpg',
-    linked_in: ''
+    linked_in: 'https://www.linkedin.com/in/bradhardin/'
+
   }, {
     name: 'Peter Hirshberg',
     title: 'Principal, Maker City Project',
     subtitle: '',
     img_path: '/images/team/hirshberg.jpg',
-    linked_in: ''
+    linked_in: 'https://www.linkedin.com/in/hirshberg/'
   }, {
     name: 'Thomas Hardjono',
     title: 'Technical Director, MIT Internet Trust Consortium',
     subtitle: '',
     img_path: '/images/team/hardjono.jpg',
-    linked_in: ''
+    linked_in: 'https://www.linkedin.com/in/hardjono/'
   }, {
     name: 'Curt Lefabvre',
     title: 'CEO, nDimensional',
     subtitle: '',
     img_path: '/images/team/lefabvre.jpg',
-    linked_in: ''
+    linked_in: 'https://www.linkedin.com/in/curt-lefebvre/'
   }, {
     name: 'Samuel Visscher',
     title: 'Crypto Assets Group',
     subtitle: '',
     img_path: '/images/team/visscher.jpg',
-    linked_in: ''
+    linked_in: 'https://www.linkedin.com/in/samuelvisscher/'
   }, {
     name: 'Robert Kastelein',
     title: 'Blockchain News',
     subtitle: '',
     img_path: '/images/team/kastelein.jpg',
-    linked_in: ''
+    linked_in: 'https://www.linkedin.com/in/expathos/'
   }]
 };
 //# sourceMappingURL=team.js.map
